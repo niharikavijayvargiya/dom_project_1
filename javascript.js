14d824c6f7b0dee37412d8dd64642b4972674c6e
Apple <apple@Apples-MacBook-Pro.local>
2024-05-22 22:51:15 +0530

    added dom project changes

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const myDiv = document.getElementById('myDiv');
+
+myDiv.innerHTML = "<b>Hello, I'm a div!</b>";
+
+myDiv.style.backgroundColor = 'yellow';
+
+myDiv.style.position = 'absolute';
+myDiv.style.top = '0';
+myDiv.style.left = '0';
+
+myDiv.style.margin = '10px';
+myDiv.style.padding = '20px';
+
+myDiv.style.fontSize = '24px';
+myDiv.style.fontWeight = 'bold';
+
+myDiv.style.height = '100px';
+myDiv.style.width = '300px';
